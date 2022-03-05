251569a5082202762a7e10619708f5b995328d6a
juanjesus <jrodbrie@gobiernodecanarias.org>
2022-03-05 23:24:23 +0000

    Esta será la primera versión del software

---
+++ src/main/java/com/mycompany/tarea1/Operaciones.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.tarea1;
+
+
+public class Operaciones {
+    
+    public Operaciones() {
+    };
+    
+    public void operacion1 (double num1, double num2) {
+        double resultado;
+        resultado = num1 * num2;
+        System.out.println("El resultado es: " + resultado);
+    }
+    
+    public void operacion2 (double importe) {
+        double resultado;
+        resultado = importe * 0.2;
+        System.out.println("El resultado es: " + resultado);
+    }
+}
---
+++ src/main/java/com/mycompany/tarea1/Principal.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.tarea1;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        Operaciones Oper = new Operaciones();
+        Oper.operacion1(4, 2);
+        Oper.operacion2(200);
+    }  
+}
